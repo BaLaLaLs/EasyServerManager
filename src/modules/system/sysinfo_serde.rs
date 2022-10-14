2c801a5dde8a9d205a182b8eb56355e13bf06871
刘顺 <909275705@qq.com>
2022-10-14 15:45:26 +0800

    完善 进程相关的数据字段

--- src/modules/system/sysinfo_serde.rs
+++ src/modules/system/sysinfo_serde.rs
@@ -1,6 +1,7 @@
+use std::ops::Deref;
 use serde::{Serialize, Serializer};
 use serde::ser::SerializeMap;
-use sysinfo::{Cpu, CpuExt, Disk, DiskExt, DiskType, LoadAvg, Networks, NetworksExt, Pid, PidExt, Process, ProcessExt, Uid};
+use sysinfo::{Cpu, CpuExt, Disk, DiskExt, DiskType, DiskUsage, LoadAvg, Networks, NetworksExt, Pid, PidExt, Process, ProcessExt, ProcessStatus, Uid};
 
 
 pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
@@ -87,20 +88,29 @@ impl<'a> Serialize for Ser<'a, Process> {
         map.serialize_entry("cwd", &self.0.cwd())?;
         map.serialize_entry("root", &self.0.root())?;
         map.serialize_entry("memory", &self.0.memory())?;
-        // map.serialize_entry("user_id", &Ser::new(self.0.user_id().unwrap()))?;
-        // map.serialize_entry("cmd", &self.0.cmd())?;
-        // map.serialize_entry("cmd", &self.0.cmd())?;
-        // map.serialize_entry("cmd", &self.0.cmd())?;
+        map.serialize_entry("virtual_memory", &self.0.virtual_memory())?;
+        map.serialize_entry("start_time", &self.0.start_time())?;
+        map.serialize_entry("run_time", &self.0.start_time())?;
+        map.serialize_entry("cpu_usage", &self.0.cpu_usage())?;
+        map.serialize_entry("disk_usage", &Ser::new(&self.0.disk_usage()))?;
+        // map.serialize_entry("user_id", &Ser::new(&self.0.user_id()))?;
+        map.serialize_entry("parent", &Ser::new(&self.0.parent()))?;
+        map.serialize_entry("pid", &self.0.pid().as_u32())?;
+        map.serialize_entry("status", &Ser::new(&self.0.status()))?;
         // map.serialize_entry("cmd", &self.0.cmd())?;
 
         map.end()
     }
 }
-impl<'a> Serialize for Ser<'a, Pid> {
+impl<'a> Serialize for Ser<'a, Option<Pid>> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where S: Serializer
     {
-        self.0.as_u32().serialize(serializer)
+        if let Some(pid) = self.0.to_owned()  {
+            return pid.as_u32().serialize(serializer)
+        }
+        return "".to_owned().serialize(serializer)
+
     }
 }
 impl<'a> Serialize for Ser<'a, Uid> {
@@ -108,5 +118,25 @@ impl<'a> Serialize for Ser<'a, Uid> {
         where S: Serializer
     {
         self.0.serialize(serializer)
+    }
+}
+impl<'a> Serialize for Ser<'a, ProcessStatus> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        self.0.to_string().serialize(serializer)
+    }
+}
+
+impl<'a> Serialize for Ser<'a, DiskUsage> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        let mut map = serializer.serialize_map(None)?;
+        map.serialize_entry("total_written_bytes", &self.0.total_written_bytes)?;
+        map.serialize_entry("written_bytes", &self.0.written_bytes)?;
+        map.serialize_entry("total_read_bytes", &self.0.total_read_bytes)?;
+        map.serialize_entry("read_bytes", &self.0.read_bytes)?;
+        map.end()
     }
 }
